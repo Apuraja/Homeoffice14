a324a8dd0245c48f8044dedaf272d1415e566a81
Apuraja <apupatel17@gmail.com>
2023-02-15 22:28:44 +0000

    Homeoffice14

---
+++ src/main/java/gov/uk/check/visa/pages/FamilyImmigrationStatusPage.java
@@ -0,0 +1,24 @@
+package gov.uk.check.visa.pages;
+
+import gov.uk.check.visa.utility.Utility;
+
+public class FamilyImmigrationStatusPage extends Utility {
+//    private static final Logger log = LogManager.getLogger(FamilyImmigrationStatusPage.class.getName());
+//
+//    public FamilyImmigrationStatusPage() {
+//        PageFactory.initElements(driver, this);
+//    }
+//
+//
+//    @CacheLookup
+//    @FindBy(xpath = "//input[@id='response=0']")
+//    WebElement yesRadiobtn;
+//
+//
+//
+//
+//    public void selectImmigrationStatus()  {
+//        clickOnElement(yesRadiobtn);
+//
+//    }
+}
---
+++ target/cucumber-report/cucumber.html/report.js
@@ -0,0 +1,399 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("visa.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Visa Test",
+  "description": "",
+  "id": "visa-test",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@Regression"
+    }
+  ]
+});
+formatter.before({
+  "duration": 6248547600,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "anAustralianCominToUKForTourism",
+  "description": "",
+  "id": "visa-test;anaustraliancomintoukfortourism",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "I am on Homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I accept all cookie",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "I click on Start Now",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I select country as Australia",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I click on Continue button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I select reason",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "I should verify Result",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "VisaConirmationTest.iAmOnHomepage()"
+});
+formatter.result({
+  "duration": 191775900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iAcceptAllCookie()"
+});
+formatter.result({
+  "duration": 143639700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnStartNow()"
+});
+formatter.result({
+  "duration": 451165100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iSelectCountryAsAustralia()"
+});
+formatter.result({
+  "duration": 172675900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnContinueButton()"
+});
+formatter.result({
+  "duration": 327985600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iSelectReason()"
+});
+formatter.result({
+  "duration": 103867400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 308978100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iShouldVerifyResult()"
+});
+formatter.result({
+  "duration": 52363100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 839469600,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 3190363300,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "aChileanComingToTheUKForWorkAndPlansOnStayingForLongerThanSixMonths",
+  "description": "",
+  "id": "visa-test;achileancomingtotheukforworkandplansonstayingforlongerthansixmonths",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 15,
+  "name": "I am on Homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 16,
+  "name": "I accept all cookie",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 17,
+  "name": "I click on Start Now",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "I select country as Chile",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "I click on Continue button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 20,
+  "name": "I select reason work or business",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 22,
+  "name": "I click on duration option",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 23,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 24,
+  "name": "I select Job type",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "I should verify Result for visa",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "VisaConirmationTest.iAmOnHomepage()"
+});
+formatter.result({
+  "duration": 29600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iAcceptAllCookie()"
+});
+formatter.result({
+  "duration": 122381600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnStartNow()"
+});
+formatter.result({
+  "duration": 373928600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iSelectCountryAsChile()"
+});
+formatter.result({
+  "duration": 199960200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnContinueButton()"
+});
+formatter.result({
+  "duration": 350281700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iSelectReasonWorkOrBusiness()"
+});
+formatter.result({
+  "duration": 79067000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 295593900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnDurationOption()"
+});
+formatter.result({
+  "duration": 80040900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 293829900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iSelectJobType()"
+});
+formatter.result({
+  "duration": 81361900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 459343200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iShouldVerifyResultForVisa()"
+});
+formatter.result({
+  "duration": 53546900,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 731921000,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 2403197100,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 28,
+  "name": "aColumbianNationalComingToTheUKToJoinAPartnerForALongStayTheyDoHaveAnArticle10Or20Card",
+  "description": "",
+  "id": "visa-test;acolumbiannationalcomingtotheuktojoinapartnerforalongstaytheydohaveanarticle10or20card",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 29,
+  "name": "I am on Homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 30,
+  "name": "I accept all cookie",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 31,
+  "name": "I click on Start Now",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "I select country as Colombia",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 33,
+  "name": "I click on Continue button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 34,
+  "name": "I select reason join partner or family",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 35,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 36,
+  "name": "I should verify Result page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "VisaConirmationTest.iAmOnHomepage()"
+});
+formatter.result({
+  "duration": 33400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iAcceptAllCookie()"
+});
+formatter.result({
+  "duration": 136254500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnStartNow()"
+});
+formatter.result({
+  "duration": 437117500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iSelectCountryAsColombia()"
+});
+formatter.result({
+  "duration": 158182800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnContinueButton()"
+});
+formatter.result({
+  "duration": 328802400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iSelectReasonJoinPartnerOrFamily()"
+});
+formatter.result({
+  "duration": 74597900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 324146100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "VisaConirmationTest.iShouldVerifyResultPage()"
+});
+formatter.result({
+  "duration": 40135000,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 779899800,
+  "status": "passed"
+});
+});
